42cac683e53081635bb5593ed0a29011f1768c5b
jv-juven <juven-jv@foxmail.com>
2016-11-03 21:41:22 +0800

    新建立项目

---
+++ app/Http/Controllers/RepairController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RepairController extends Controller
+{
+    public function __construct() {}
+
+    // 报修页面提交表单
+    public function postRepair(Request $rq) {}
+}
